abe01ffca50a8d49b850dc204bb2f50ec105c4bc
Ray Feng <ray@dotdothorse.com>
2025-07-13 22:49:47 +1000

    Updated sitemap url

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://liftdskin.com.au',
+  site: 'https://www.liftdskin.com.au',
   output: 'static',
 
   integrations: [
